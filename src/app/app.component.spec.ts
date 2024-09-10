bd6babf183a56d897ce4db186e96714ad2c913fe
Charmy <charmytseng0118@gmail.com>
2024-09-11 00:34:44 +0800

    Modify Angular-Test-Deploy to Test-Angular-Deploy

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular-Test-Deploy' title`, () => {
+  it(`should have the 'Test-Angular-Deploy' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-Test-Deploy');
+    expect(app.title).toEqual('Test-Angular-Deploy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-Test-Deploy');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Test-Angular-Deploy');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Angular-Test-Deploy';
+  title = 'Test-Angular-Deploy';
 }
